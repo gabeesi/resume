d1fc08120505339fca335e273f50265458a526aa
Gabriela Nunes <gabih.1111@hotmail.com>
2026-03-30 12:53:28 -0300

    att designs

--- designs.js
+++ designs.js
@@ -34,33 +34,3 @@ document.querySelectorAll(".carrossel").forEach(carousel => {
 
 });
 
-const modal = document.getElementById("imageModal");
-const modalImg = document.getElementById("modalImg");
-const closeBtn = document.querySelector(".close");
-
-document.querySelectorAll(".carrossel").forEach(carousel => {
-
-    const modal = document.getElementById("imageModal");
-    const modalImg = document.getElementById("modalImg");
-
-    carousel.addEventListener("click", () => {
-        const activeImg = carousel.querySelector(".carousel-img.active");
-        
-        if (activeImg) {
-            modal.style.display = "block";
-            modalImg.src = activeImg.src;
-        }
-    });
-
-});
-
-closeBtn.addEventListener("click", () => {
-        modal.style.display = "none";
-    });
-
-    modal.addEventListener("click", (e) => {
-        if (e.target === modal) {
-            modal.style.display = "none";
-        }
-    });
-
